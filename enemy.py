5b6c89a1b5a5bfdb012419f67ae6cc2e89b799a5
Samuel Toloza <tlz.system@gmail.com>
2015-09-22 02:28:42 -0300

    niveles dificultad

--- enemy.py
+++ enemy.py
@@ -6,28 +6,33 @@ NEGRO    = (0, 0, 0)
 
 class Enemy(pygame.sprite.Sprite):
 
-	rect_x = 1
+	rect_x = 10
 	rect_y = random.randint(50,500) 
 
-	rect_cambio_x = 20
-	rect_cambio_y = 20
+	rect_cambio_x = 15
+	rect_cambio_y = 15
+	enPantalla = False
 
 
 	def __init__(self):
 		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.image.load("art/ball.png").convert()
+		self.image = pygame.image.load("art/ball.jpg").convert()
 		self.image.set_colorkey(BLANCO)
 		self.rect = self.image.get_rect()
+		self.enPantalla = False
+		self.rect.x=1600
 
 	def aparecer(self):
 		self.rect.x=self.rect_x
 		self.rect.y=self.rect_y
+		self.enPantalla = True
 
 	def mover(self):
-		if self.rect.y > 550 or self.rect.y <0:
-			self.rect_cambio_y= self.rect_cambio_y * -1
-		if self.rect.x > 1150 or self.rect.x <0:
-			self.rect_cambio_x = self.rect_cambio_x*-1
-		self.rect.x+=self.rect_cambio_x
-		self.rect.y+=self.rect_cambio_y
+		if self.enPantalla == True:
+			if self.rect.y > 550 or self.rect.y <0:
+				self.rect_cambio_y= self.rect_cambio_y * -1
+			if self.rect.x > 1150 or self.rect.x <0:
+				self.rect_cambio_x = self.rect_cambio_x*-1
+			self.rect.x+=self.rect_cambio_x
+			self.rect.y+=self.rect_cambio_y
 
--- main.py
+++ main.py
@@ -15,21 +15,26 @@ class Juego(object):
 	lista_coin = None
 	lista_enemigos = None
 
+	states=["welcome","game","gameover"]
+	current_state = states[0]
 	game_over = False
 	pantalla_principal = True
 	puntos = 0
 	imagen_fondo = None 
 	imagen_gameover = None
 	moneda = None
-	enemigo = None
+
 	fuente = None
 	sonido_comer = None
 	sonido_morir = None
+	nivel = 0
+
 
 
 
 	def __init__(self):
 		self.puntos = 0
+		self.nivel = 0
 		self.game_over = False
 		self.lista_todos= pygame.sprite.Group()
 		self.lista_coin = pygame.sprite.Group()
@@ -38,14 +43,20 @@ class Juego(object):
 
 		self.jugador = Player()
 		self.moneda = Coin()
-		self.enemigo = Enemy()
+
+
+# Carga de enemigos. 3 maximos.
+		for i in range(3):
+			self.enemigo = Enemy()
+			self.lista_enemigos.add(self.enemigo)
+			
 
 
 		self.lista_todos.add(self.jugador)
 		self.lista_todos.add(self.moneda)
-		self.lista_todos.add(self.enemigo)
+		
 		self.lista_coin.add(self.moneda)
-		self.lista_enemigos.add(self.enemigo)
+		
 
 		self.imagen_fondo = pygame.image.load('art/start_screen.jpg')
 		self.imagen_gameover = pygame.image.load('art/game_over.jpg')
@@ -61,18 +72,19 @@ class Juego(object):
 				return True
 			if evento.type == pygame.KEYDOWN:
 				if evento.key == pygame.K_RETURN:  
-					if self.game_over==False:
+					if self.current_state==self.states[0]:
 						self.__init__()
-						self.pantalla_principal = False
 						self.moneda.aparecer()
-						self.enemigo.aparecer()
+						self.lista_enemigos.sprites()[self.nivel].aparecer()
+						self.lista_todos.add(self.lista_enemigos.sprites()[self.nivel])
+						self.current_state = self.states[1]
 
-					if self.game_over ==True:
+					if self.current_state==self.states[2]:
 						self.__init__()
-						self.pantalla_principal = False
-						self.moneda.aparecer()
-						self.enemigo.aparecer()
-						self.game_over=False
+						self.moneda.aparecer()						
+						self.lista_enemigos.sprites()[self.nivel].aparecer()
+						self.lista_todos.add(self.lista_enemigos.sprites()[self.nivel])
+						self.current_state=self.states[1]
 						    
 
 		return False
@@ -80,18 +92,28 @@ class Juego(object):
 
 	def game_engine(self):
 
-		if not self.game_over and self.pantalla_principal==False:
+		if self.current_state==self.states[1]:
 			self.lista_todos.update()
-			self.enemigo.mover()
+			for i in range(self.nivel + 1):
+				self.lista_enemigos.sprites()[i].mover()
 			impacto = pygame.sprite.spritecollide(self.jugador,self.lista_coin,False)
 			for self.moneda in impacto:
 				self.puntos = self.puntos +1
 				self.sonido_comer.play()
 				self.moneda.aparecer()
+			if self.puntos ==20 and self.nivel == 0:
+				self.nivel = self.nivel + 1
+				self.lista_enemigos.sprites()[self.nivel].aparecer()
+				self.lista_todos.add(self.lista_enemigos.sprites()[self.nivel])
+			if self.puntos ==30 and self.nivel == 1:
+				self.nivel = self.nivel + 1
+				self.lista_enemigos.sprites()[self.nivel].aparecer()
+				self.lista_todos.add(self.lista_enemigos.sprites()[self.nivel])
+
 			impacto_enemigo = pygame.sprite.spritecollide(self.jugador, self.lista_enemigos, True)
 			for self.enemigo in impacto_enemigo:
 				self.sonido_morir.play()
-				self.game_over = True
+				self.current_state=self.states[2]
 
 
 			
@@ -100,23 +122,24 @@ class Juego(object):
 	def display_frame(self,panta):
 
 		panta.fill(BLANCO)
-
-
 		
-		if self.pantalla_principal == True:
+		if self.current_state == self.states[0]:
 			panta.blit(self.imagen_fondo,(0,0))
 
-		if  self.pantalla_principal == False:
+		if  self.current_state == self.states[1]:
 			panta.fill(BLANCO)
 			self.lista_todos.draw(panta)
 			texto_salida = str(self.puntos)+" Points"
 			texto = self.fuente.render(texto_salida,True,NEGRO)
 			panta.blit(texto,[10,500])
+			texto_nivel = "Level "+str(self.nivel +1)
+			textop = self.fuente.render(texto_nivel,True,NEGRO)
+			panta.blit(textop,[590,20])
 
 
-		if self.game_over == True:
+		if self.current_state == self.states[2]:
 			panta.blit(self.imagen_gameover,(0,0))
-			texto_salida = "You lose :( .  Yoy score "+str(self.puntos)+" point"
+			texto_salida = "You lose :( .  You score "+str(self.puntos)+" points"
 			texto = self.fuente.render(texto_salida,True,NEGRO)
 			panta.blit(texto,[10,500])
 		pygame.display.flip()
